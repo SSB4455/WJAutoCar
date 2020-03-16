e5812621076552be3eeac7f25ccd45fc0e02eb64
SSB4455 <741086@gmail.com>
2020-03-16 23:54:27 +0800

    抽出车辆控制和代理学习奖惩逻辑 修复新地图的路面缝隙

--- Assets/WJAutoCar/WJAutoCarAgent.cs
+++ Assets/WJAutoCar/WJAutoCarAgent.cs
@@ -3,57 +3,35 @@ using MLAgents;
 
 public class WJAutoCarAgent : Agent
 {
-	public float motorMax, steerMax;
-
-	private Vector3 forward;
-	private WheelCollider fl, fr, hl, hr;
+	private WheelDrive wd;
+	private WheelCollider fl, hl;
 
 
 
 	void Start()
 	{
-		Debug.Log("Start");
-
-		fl = transform.Find("Alloys01").Find("fl").GetComponent<WheelCollider>();
-		fr = transform.Find("Alloys01").Find("fr").GetComponent<WheelCollider>();
-		hl = transform.Find("Alloys01").Find("hl").GetComponent<WheelCollider>();
-		hr = transform.Find("Alloys01").Find("hr").GetComponent<WheelCollider>();
-
-		GetComponent<Rigidbody>().centerOfMass = new Vector3(0, -0.01f, -0.02f);//左右 上下 前后
-
 		Monitor.SetActive(true);
 
+		//GetComponent<Rigidbody>().centerOfMass = new Vector3(0, -0.01f, -0.02f);//左右 上下 前后
+		wd = transform.GetComponent<WheelDrive>();
+		wd.scriptControl = true;
 
-		//GetComponentInChildren<Collider>().private void OnTriggerEnter(Collider other) {
-			
-		//}
-
+		fl = transform.Find("Alloys01").Find("fl").GetComponent<WheelCollider>();
+		hl = transform.Find("Alloys01").Find("hl").GetComponent<WheelCollider>();
 	}
 
 	public override void AgentReset()
 	{
 		Debug.Log("AgentReset");
 
-		//DestroyImmediate(car);
-		//car = Instantiate(prefab, new Vector3(0.0f, 1.0f, 0.0f), Quaternion.Euler(0.0f, 0.0f, 0.0f));
-
 		GetComponent<Rigidbody>().velocity = Vector3.zero;
 		GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
 		transform.position = new Vector3(0, 1, 0);
 		transform.rotation = Quaternion.Euler(0, 0, 0);
 
-		fl.steerAngle = 0;
-		fr.steerAngle = 0;
-		
-		fl.motorTorque = 0;
-		fr.motorTorque = 0;
-		hl.motorTorque = 0;
-		hr.motorTorque = 0;
+		wd.Drive(0, 0);
 
 		//hr.brakeTorque = 10000;
-
-
-		forward = Vector3.Normalize(fl.transform.position - hl.transform.position);
 		
 		lastCollisionTime = Time.realtimeSinceStartup;
 	}
@@ -71,13 +49,21 @@ public class WJAutoCarAgent : Agent
     
 	public override void AgentAction(float[] vectorAction)
 	{
+		wd.Drive(vectorAction[0], vectorAction[1]);
+		AddReward(-0.01f);
 
-		Monitor.Log("vectorAction", vectorAction, transform);
+		float[] desplayTorque = new float[] { vectorAction[1] };
+		Monitor.Log("torque", desplayTorque, transform);
+		Monitor.Log("steer", vectorAction[0], transform);
 		//Monitor.Log("vectorAction", vectorAction[0], null);
 		Monitor.Log("CumulativeReward", this.GetCumulativeReward(), null);
-		Monitor.Log("time left", this.maxStep - this.GetStepCount(), null);
-		
-		//this.rew
+		Monitor.Log("time left", (this.maxStep - this.GetStepCount()) / (float)this.maxStep, null);
+
+		//Debug.Log("torque = " + torque + " forwardSpeed = " + forwardSpeed);
+	}
+
+	void Update()
+	{
 		float angle_z = transform.rotation.eulerAngles.z > 180 ? 360 - transform.rotation.eulerAngles.z : transform.rotation.eulerAngles.z;
 		//翻车
 		if (Mathf.Abs(angle_z) > 20)
@@ -94,26 +80,9 @@ public class WJAutoCarAgent : Agent
 			Done();
 		}
 
-		float steer = vectorAction[0] * steerMax;
-		float torque = motorMax * -vectorAction[1];
-		hl.motorTorque = torque;
-		hr.motorTorque = torque;
-		Vector3 position;
-		Quaternion rotation;
-
-		fl.steerAngle = steer;
-		fr.steerAngle = steer;
-		fl.GetWorldPose(out position, out rotation);
-		fl.transform.rotation = rotation;
-		fr.transform.rotation = rotation;
-		hr.GetWorldPose(out position, out rotation);
-		hl.transform.rotation = rotation;
-		hr.transform.rotation = rotation;
-
-		forward = Vector3.Normalize(fl.transform.position - hl.transform.position);
-		float forwardSpeed = Vector3.Dot(forward, GetComponent<Rigidbody>().velocity);
+		float forwardSpeed = Vector3.Dot(transform.forward, GetComponent<Rigidbody>().velocity);
+		Monitor.Log("forwardSpeed", forwardSpeed / 30, transform);
 		AddReward(forwardSpeed / 1000);
-		//Debug.Log("torque = " + torque + " forwardSpeed = " + forwardSpeed);
 	}
 
 	public override float[] Heuristic()
---
+++ Assets/WJAutoCar/WheelDrive.cs
@@ -0,0 +1,120 @@
+﻿using UnityEngine;
+using System;
+
+[Serializable]
+public enum DriveType
+{
+	RearWheelDrive,
+	FrontWheelDrive,
+	AllWheelDrive
+}
+
+public class WheelDrive : MonoBehaviour
+{
+    [Tooltip("Maximum steering angle of the wheels")]
+	public float maxAngle = 30f;
+	[Tooltip("Maximum torque applied to the driving wheels")]
+	public float maxTorque = 300f;
+	[Tooltip("Maximum brake torque applied to the driving wheels")]
+	public float brakeTorque = 30000f;
+	[Tooltip("If you need the visual wheels to be attached automatically, drag the wheel shape here.")]
+	public GameObject wheelShape;
+
+	[Tooltip("The vehicle's speed when the physics engine can use different amount of sub-steps (in m/s).")]
+	public float criticalSpeed = 5f;
+	[Tooltip("Simulation sub-steps when the speed is above critical.")]
+	public int stepsBelow = 5;
+	[Tooltip("Simulation sub-steps when the speed is below critical.")]
+	public int stepsAbove = 1;
+
+	[Tooltip("The vehicle's drive type: rear-wheels drive, front-wheels drive or all-wheels drive.")]
+	public DriveType driveType;
+	public bool scriptControl;
+
+    private WheelCollider[] m_Wheels;
+
+
+
+    // Find all the WheelColliders down in the hierarchy.
+	void Start()
+	{
+		m_Wheels = GetComponentsInChildren<WheelCollider>();
+
+		for (int i = 0; i < m_Wheels.Length; ++i) 
+		{
+			var wheel = m_Wheels [i];
+
+			// Create wheel shapes only when needed.
+			if (wheelShape != null)
+			{
+				var ws = Instantiate (wheelShape);
+				ws.transform.parent = wheel.transform;
+			}
+		}
+	}
+
+	// This is a really simple approach to updating wheels.
+	// We simulate a rear wheel drive car and assume that the car is perfectly symmetric at local zero.
+	// This helps us to figure our which wheels are front ones and which are rear.
+	void Update()
+	{
+		if (!scriptControl)
+		{
+			Drive(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+		}
+	}
+
+	public void Drive(float angle, float torque, bool pullHandBrake = false)
+	{
+		//m_Wheels[0].ConfigureVehicleSubsteps(criticalSpeed, stepsBelow, stepsAbove);
+
+		angle = maxAngle * angle;
+		torque = maxTorque * torque;
+		float handBrake = pullHandBrake ? brakeTorque : 0;
+		Vector3 position;
+		Quaternion rotation;
+
+		foreach (WheelCollider wheel in m_Wheels)
+		{
+			// A simple car where front wheels steer while rear ones drive.
+			if (wheel.transform.localPosition.z > 0)
+			{
+				wheel.steerAngle = angle;
+				wheel.GetWorldPose(out position, out rotation);
+				wheel.transform.rotation = rotation;
+				wheel.transform.rotation = rotation;
+			}
+
+			if (wheel.transform.localPosition.z < 0)
+			{
+				wheel.brakeTorque = handBrake;
+			}
+
+			if (wheel.transform.localPosition.z < 0 && driveType != DriveType.FrontWheelDrive)
+			{
+				wheel.motorTorque = torque;
+				wheel.GetWorldPose(out position, out rotation);
+				wheel.transform.rotation = rotation;
+				wheel.transform.rotation = rotation;
+			}
+
+			if (wheel.transform.localPosition.z >= 0 && driveType != DriveType.RearWheelDrive)
+			{
+				wheel.motorTorque = torque;
+			}
+
+			// Update visual wheels if any.
+			if (wheelShape) 
+			{
+				Quaternion q;
+				Vector3 p;
+				wheel.GetWorldPose (out p, out q);
+
+				// Assume that the only child of the wheelcollider is the wheel shape.
+				Transform shapeTransform = wheel.transform.GetChild (0);
+				shapeTransform.position = p;
+				shapeTransform.rotation = q;
+			}
+		}
+	}
+}
